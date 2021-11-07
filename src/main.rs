52648f05d97a7953bde7d084975ccc63fcec79b2
matteopolak <matteopolak@hotmail.com>
2021-11-07 12:51:55 -0500

    lazy_static global variables

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::{
 	fs::{self, File},
 	io::{BufRead, BufReader},
-	path::Path,
+	path::{Path, PathBuf},
 	process::{Command, Stdio},
 	thread, time,
 };
@@ -18,17 +21,35 @@ static MACHINE: &str = "ubuntu";
 #[cfg(target_os = "windows")]
 static MACHINE: &str = "win10";
 
-fn submit_answer(answer: &str, removed: bool, path: &Path, team_id: &str) {
-	Command::new(path)
+#[cfg(target_os = "windows")]
+lazy_static! {
+	static ref REPLACER: Regex = Regex::new("\\s*=\\s*").unwrap();
+	static ref BASE: PathBuf = Path::new("C:\\CyberPatriot").to_path_buf();
+	static ref BIN: PathBuf = BASE.join("CCSCheck.exe").to_path_buf();
+	static ref MACHINE_UID: String = machine_uid::get().unwrap();
+	static ref TEAM_ID: String = std::env::var("TEAM_ID").unwrap_or("0123-0123-0123".to_string());
+}
+
+#[cfg(target_os = "linux")]
+lazy_static! {
+	static ref REPLACER: Regex = Regex::new("\\s*=\\s*").unwrap();
+	static ref BASE: PathBuf = Path::new("/opt/CyberPatriot").to_path_buf();
+	static ref BIN: PathBuf = BASE.join("CCSCheck").to_path_buf();
+	static ref MACHINE_UID: String = machine_uid::get().unwrap();
+	static ref TEAM_ID: String = std::env::var("TEAM_ID").unwrap_or("0123-0123-0123".to_string());
+}
+
+fn submit_answer(answer: &str, removed: bool) {
+	Command::new(BIN.as_os_str())
 		.arg(answer)
 		.arg(if removed { "1" } else { "0" })
-		.env("TEAM_ID", team_id)
+		.env("TEAM_ID", TEAM_ID.as_str())
 		.spawn()
 		.ok();
 }
 
-fn format_answer(answer: &str, replacer: &Regex) -> String {
-	let replaced = replacer.replace_all(answer.trim(), "=");
+fn format_answer(answer: &str) -> String {
+	let replaced = REPLACER.replace_all(answer.trim(), "=");
 	let lower = replaced.to_lowercase();
 	let mut data = lower.split_whitespace().into_iter().collect::<Vec<&str>>();
 
@@ -37,14 +58,14 @@ fn format_answer(answer: &str, replacer: &Regex) -> String {
 	data.join(" ")
 }
 
-fn check_file(path: &Path, bin: &Path, base: &Path, replacer: &Regex, team_id: &str) {
+fn check_file(path: &Path) {
 	let path_str = path.to_str().unwrap();
 	let mut hasher = Sha512::new();
 
 	hasher.update(path_str);
 
 	let hash = hex::encode(hasher.finalize_reset());
-	let stored_path = base.join("diffs").join(hash.as_str());
+	let stored_path = BASE.join("diffs").join(hash.as_str());
 
 	let stored_exists = stored_path.exists();
 	let new_exists = path.exists();
@@ -59,24 +80,24 @@ fn check_file(path: &Path, bin: &Path, base: &Path, replacer: &Regex, team_id: &
 		false => "".to_string(),
 	};
 
-	check_diff(&old, &new, path_str, bin, replacer, team_id);
+	check_diff(&old, &new, path_str);
 
 	if !new_exists {
 		fs::remove_file(stored_path).ok();
 
 		if stored_exists {
-			submit_answer(path_str, true, bin, team_id);
+			submit_answer(path_str, true);
 		}
 	} else {
 		fs::write(stored_path, new).unwrap();
 
 		if !stored_exists {
-			submit_answer(path_str, false, bin, team_id);
+			submit_answer(path_str, false);
 		}
 	}
 }
 
-fn check_diff(old: &String, new: &String, path_str: &str, bin: &Path, replacer: &Regex, team_id: &str) {
+fn check_diff(old: &String, new: &String, path_str: &str) {
 	let diff = TextDiff::from_lines(old, new);
 
 	for change in diff.iter_all_changes() {
@@ -92,10 +113,10 @@ fn check_diff(old: &String, new: &String, path_str: &str, bin: &Path, replacer: 
 			continue;
 		}
 
-		#[cfg(target_os = "linux")]
+		//#[cfg(target_os = "linux")]
 		if path_str == "/etc/passwd" {
 			if let Some(username) = line.split(':').next() {
-				submit_answer(&format!("{}{}", username, path_str), removed, bin);
+				submit_answer(&format!("{}{}", username, path_str), removed);
 			}
 		} else if path_str == "/etc/group" {
 			let mut split = line.split(':');
@@ -103,23 +124,21 @@ fn check_diff(old: &String, new: &String, path_str: &str, bin: &Path, replacer: 
 			if let Some(group) = split.next() {
 				if let Some(users) = split.nth(2) {
 					for user in users.split(',') {
-						submit_answer(&format!("{}:{}{}", group, user, path_str), removed, bin);
+						submit_answer(&format!("{}:{}{}", group, user, path_str), removed);
 					}
 				}
 			}
 		}
 
 		submit_answer(
-			&format!("{}{}", format_answer(line, &replacer), path_str),
-			removed,
-			bin,
-			team_id
+			&format!("{}{}", format_answer(line), path_str),
+			removed
 		);
 	}
 }
 
-fn check_processes(base: &Path, bin: &Path, replacer: &Regex, team_id: &str) {
-	let path = base.join("diffs").join("processes");
+fn check_processes() {
+	let path = BASE.join("diffs").join("processes");
 
 	#[cfg(target_os = "linux")]
 	let output = Command::new("/bin/bash")
@@ -139,14 +158,14 @@ fn check_processes(base: &Path, bin: &Path, replacer: &Regex, team_id: &str) {
 	let new = String::from_utf8(output.stdout).unwrap();
 	let old = fs::read_to_string(&path).unwrap();
 
-	check_diff(&old, &new, "", bin, replacer, team_id);
+	check_diff(&old, &new, "");
 
 	fs::write(path, new).unwrap();
 }
 
 #[cfg(target_os = "linux")]
-fn check_packages(base: &Path, bin: &Path, replacer: &Regex, team_id: &str) {
-	let path = base.join("diffs").join("packages");
+fn check_packages() {
+	let path = BASE.join("diffs").join("packages");
 
 	let output = Command::new("/bin/bash")
 		.arg("-c")
@@ -158,16 +177,16 @@ fn check_packages(base: &Path, bin: &Path, replacer: &Regex, team_id: &str) {
 	let new = String::from_utf8(output.stdout).unwrap();
 	let old = fs::read_to_string(&path).unwrap();
 
-	check_diff(&old, &new, "", bin, replacer, team_id);
+	check_diff(&old, &new, "");
 
 	fs::write(path, new).unwrap();
 }
 
-fn send_ping(machine_uid: &str, team_id: &str, client: &reqwest::blocking::Client) -> bool {
+fn send_ping(client: &reqwest::blocking::Client) -> bool {
 	if let Ok(result) = client
 		.head(format!(
 			"http://192.168.1.129:3000/ping?machine={}&uid={}&id={}",
-			MACHINE, machine_uid, team_id
+			MACHINE, MACHINE_UID.as_str(), TEAM_ID.as_str()
 		))
 		.send()
 	{
@@ -194,25 +213,9 @@ fn self_destruct() {
 }
 
 fn main() {
-	#[cfg(target_os = "windows")]
-	let base = Path::new("C:\\CyberPatriot");
-
-	#[cfg(target_os = "windows")]
-	let bin = base.join("CCSCheck.exe");
-
-	#[cfg(target_os = "linux")]
-	let base = Path::new("/opt/CyberPatriot");
-
-	#[cfg(target_os = "linux")]
-	let bin = base.join("CCSCheck");
-
-	let machine_uid = machine_uid::get().unwrap();
 	let client = reqwest::blocking::Client::new();
-	let team_id = std::env::var("TEAM_ID").unwrap_or("0123-0123-0123".to_string());
 
-	let replacer = Regex::new("\\s*=\\s*").unwrap();
-
-	let file = File::open(base.join("files.dat")).unwrap();
+	let file = File::open(BASE.join("files.dat")).unwrap();
 	let files: Vec<String> = BufReader::new(file).lines().map(|l| l.unwrap()).collect();
 
 	let sleep_time = time::Duration::from_secs(60);
@@ -225,19 +228,19 @@ fn main() {
 	}
 
 	loop {
-		if send_ping(&machine_uid, &team_id, &client) {
+		if send_ping(&client) {
 			self_destruct();
 
 			std::process::exit(1);
 		}
 
-		check_processes(base, &bin, &replacer, &team_id);
+		check_processes();
 
 		#[cfg(target_os = "linux")]
-		check_packages(base, &bin, &replacer, &team_id);
+		check_packages();
 
 		for file in &files {
-			check_file(Path::new(file), &bin, base, &replacer, &team_id);
+			check_file(Path::new(file));
 		}
 
 		thread::sleep(sleep_time);
